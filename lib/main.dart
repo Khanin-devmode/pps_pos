a2985891842d71a43784ef536b471112592f78ec
Khanin <khanin.dev@gmail.com>
2023-09-09 23:23:11 +0700

    add grid view for menu

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PPS POS',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
@@ -88,12 +88,12 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           ),
-          const Expanded(
+          Expanded(
             child: Padding(
-              padding: EdgeInsets.all(60.0),
+              padding: const EdgeInsets.fromLTRB(60, 60, 60, 0),
               child: Column(
                 children: [
-                  Row(
+                  const Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
@@ -120,15 +120,55 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                   Expanded(
-                    child: Center(
-                      child: Text('This is content'),
+                    child: GridView.count(
+                      padding: EdgeInsets.only(top: 48, right: 80),
+                      crossAxisSpacing: 44,
+                      mainAxisSpacing: 44,
+                      crossAxisCount: 3,
+                      children: const <Widget>[
+                        CardMenu(),
+                        CardMenu(),
+                        CardMenu(),
+                        CardMenu(),
+                        CardMenu(),
+                        CardMenu(),
+                      ],
                     ),
-                  ),
+                  )
                 ],
               ),
             ),
           )
         ]),
+      ),
+    );
+  }
+}
+
+class CardMenu extends StatefulWidget {
+  const CardMenu({super.key});
+
+  @override
+  State<CardMenu> createState() => _CardMenuState();
+}
+
+class _CardMenuState extends State<CardMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: ShapeDecoration(
+        color: const Color(0xFF2B6DF9),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        shadows: const [
+          BoxShadow(
+            color: Color(0x19000000),
+            blurRadius: 23,
+            offset: Offset(6, 6),
+            spreadRadius: 4,
+          )
+        ],
       ),
     );
   }
